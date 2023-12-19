f1d1d7e5d4a7070e56a8e31a2fb9f37036d19a5c
Dave Rintoul <drintoul@gmail.com>
2023-12-19 18:53:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-@st.data_cache
 conn = st.experimental_connection('pets_db', type='sql')
-pet_owners = conn.query('select * from pet_owners', ttl=3600)
+
+pet_owners = conn.query('select * from pet_owners')
 
 for row in pet_owners.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
