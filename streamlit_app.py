08b1c61370e74eee1c0ab1b7a104f548c53b1d25
Dave Rintoul <drintoul@gmail.com>
2023-12-19 18:01:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+conn = st.connection('mysql', type='sql')
+
+df = conn.query('SELECT * FROM mytable;', ttl=600)
+
+for row in df.itertuples():
+  st.write(f"{row.name} has a :{row.pet}:")
