2df6dbfc19937cd6f4f96b0f3f03e6a466659aa9
Dave Rintoul <drintoul@gmail.com>
2023-12-19 18:42:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,7 @@
 import streamlit as st
 
-@st.data_cache(allow_output_mutation=True)
-def get_connection():
-    return create_engine("mssql+pyodbc://username:password@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server", 
-    fast_executemany = True
-    )
+conn = st.experimental_connection('pets_db', type='sql')
+pet_owners = conn.query('select * from pet_owners')
 
-@st.data_cache
-def read_df(q):
-  df = pd.read_sql_query(q, get_connection())
-  return df
-
-q1 = 'SELECT * from mytable;'
-df = read_df(q1)
-
-# Print results.
-for row in df.itertuples():
+for row in pet_owners.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
