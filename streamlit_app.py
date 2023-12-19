b7ec2ca72e6ebf86e95acc49dc07a70d009bb7b1
Dave Rintoul <drintoul@gmail.com>
2023-12-19 18:34:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,18 @@
 import streamlit as st
 
-# Initialize connection.
-conn = st.connection('mysql', type='sql')
+@st.data_cache(allow_output_mutation=True)
+def get_connection():
+    return create_engine("mssql+pyodbc://username:password@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server", 
+    fast_executemany = True
+    )
 
-# Perform query.
-df = conn.query('SELECT * from mytable;', ttl=600)
+@st.data_cache
+def read_df(q):
+  df = pd.read_sql_query(q, get_connection())
+  return df
+
+q1 = 'SELECT * from mytable;'
+df = read_df(q1)
 
 # Print results.
 for row in df.itertuples():
