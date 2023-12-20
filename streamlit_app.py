552b64ec0cd5a34c7483430a73252d0710e75a54
Dave Rintoul <drintoul@gmail.com>
2023-12-20 07:46:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,12 @@ import streamlit as st
 import pymysql
 import toml
 
-# Load configuration from config.toml file
-config = toml.load("config.toml")
-db_config = config["database"]
-
 # Database connection details
-host = db_config["host"]
-user = db_config["user"]
-password = db_config["password"]
-database = db_config["name"]
-port = db_config["port"]
+host = st.secrets["host"]
+user = st.secrets["user"]
+password = st.secrets["password"]
+database = st.secrets["name"]
+port = st.secrets["port"]
 
 # Connect to the database
 conn = pymysql.connect(host=host, user=user, password=password, database=database, port=port)
