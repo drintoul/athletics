1ef335d60218201de1dfde93fc614584ecf71694
Dave Rintoul <drintoul@gmail.com>
2023-12-19 21:59:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 import mysql.connector
 
-@st.cache_resource
 def init_connection():
     return mysql.connector.connect(**st.secrets["mysql"])
 
 conn = init_connection()
 
-@st.cache_data(ttl=600)
 pet_owners = conn.query("select * from pet_owners;")
 
 for row in pet_owners.itertuples():
