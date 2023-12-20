f2b64ff181b6fde98c811c6e97bf8d421275345f
Dave Rintoul <drintoul@gmail.com>
2023-12-20 08:01:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,24 +9,26 @@ password = st.secrets["database"]["password"]
 database = st.secrets["database"]["name"]
 port = st.secrets["database"]["port"]
 
+st.write(host, user, password, database, port)
+
 # Connect to the database
-conn = pymysql.connect(host=host, user=user, password=password, database=database, port=port)
-cursor = conn.cursor()
+#conn = pymysql.connect(host=host, user=user, password=password, database=database, port=port)
+#cursor = conn.cursor()
 
 # Streamlit app
-st.title("MySQL-Streamlit App")
+#st.title("MySQL-Streamlit App")
 
 # Example query
-query = "SELECT * FROM pet_owners LIMIT 5"
-cursor.execute(query)
-results = cursor.fetchall()
+#query = "SELECT * FROM pet_owners LIMIT 5"
+#cursor.execute(query)
+#results = cursor.fetchall()
 
 # Display results in Streamlit
-st.write("Results from MySQL:")
-for result in results:
-    st.write(result)
+#st.write("Results from MySQL:")
+#for result in results.itertuples():
+#    st.write(result.owner, f":{result.pet}:")
 
 # Close the database connection
-cursor.close()
-conn.close()
+#cursor.close()
+#conn.close()
 
